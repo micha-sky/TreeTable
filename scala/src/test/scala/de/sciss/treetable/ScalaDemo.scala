289a85a9b864d75296106ad1bd9e59f7f0095f6e
Sciss <contact@sciss.de>
2013-06-11 22:01:15 +0200

    ...

---
+++ scala/src/test/scala/de/sciss/treetable/ScalaDemo.scala
@@ -0,0 +1,11 @@
+package de.sciss.treetable
+
+import scala.swing.{Frame, SimpleSwingApplication}
+
+object ScalaDemo extends SimpleSwingApplication {
+  lazy val top = new Frame {
+    title = "TreeTable"
+
+
+  }
+}
